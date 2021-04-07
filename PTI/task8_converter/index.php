d04bed330185fb7a0f1bfd465da3408d90b0d61d
AntonLazarev <bnnrymndy@gmail.com>
2021-04-07 23:13:56 +0300

    added css to task7

--- PTI/task8_converter/index.php
+++ PTI/task8_converter/index.php
@@ -4,28 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/main.js"></script>
     <title>Converter 3000</title>
 </head>
 <body>
     <div class="container">
         <form action="index.php" method="get">
             <datalist id="list_currience">
-                <option label="Российский рубль" value="RUB" >
-                <option label="Евро" value="EUR">
-                <option label="Доллар США" value="USD">
-                <option label="Фунт стерлингов" value="GBP">
-                <option label="Японская иена" value="JPY">
+                <option label="🇷🇺 Российский рубль" value="RUB" >
+                <option label="🏳‍🌈 Евро" value="EUR">
+                <option label="🇺🇲 Доллар США" value="USD">
+                <option label="🇬🇧 Фунт стерлингов" value="GBP">
+                <option label="🇯🇵 Японская иена" value="JPY">
             </datalist>
-
-            <input name="from_currience" list="list_currience" value="<?php echo $_GET['from_currience']?>">
-            <input name="to_currience" list="list_currience" value="<?php echo $_GET['to_currience']?>">
+            <h1>Конвертер 3001</h1>
+            <h2>Введите Валюту</h2>
             
-            <input type="number" name="value_to_convert" id="" value="<?php echo $_GET['value_to_convert']?>">
-
-            <input type="submit" value="Ковертировать">
+            <span>Вашу: </span>
+            <input required id="from_currience" name="from_currience" list="list_currience" value="<?php echo $_GET['from_currience']?>">
+            <br><br><span>Назначения: </span>
+            <input required id="to_currience" name="to_currience" list="list_currience" value="<?php echo $_GET['to_currience']?>">
+            <br><br>
+            <span>Введите сумму: </span>
+            <input required id="value_to_convert" type="number" name="value_to_convert" id="" value="<?php echo $_GET['value_to_convert']?>">
+            <br><br>
+            <input type="submit" value="Рассчитать">
         </form>
 
-        <div class="result">
+        <h2 class="result">
             <?php
                 $curriences_values = [  
                                     'RUB' => 1.,
@@ -41,9 +48,9 @@
                 
                 $converted_value = round (($value_to_convert / $curriences_values[$from_currience]) * $curriences_values[$to_currience] , 4);
 
-                echo "$converted_value $to_currience";
+                echo "Результат:<br>$converted_value $to_currience";
             ?>
-        </div>
+        </h2>
     </div>
 </body>
 </html>
